9505abfba8f3e62de546be822161f375f840ed90
Théo Matussière <theo@matussie.re>
2022-04-14 23:06:14 +0200

    add alts

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,19 +19,19 @@ export default function Landing() {
 
       <div className="flex flex-col flex-grow gap-8 my-16 px-4 max-w-xl">
         <div className="max-w-md mx-4">
-          <Image src={insync} objectFit="contain" />
+          <Image alt="" src={insync} objectFit="contain" />
         </div>
         <h1 className="font-black text-black text-3xl sm:text-4xl text-center">
           Ritualize meeting up with friends and colleagues for workdays!
         </h1>
         <div className="max-w-md mx-4">
-          <Image src={calendar} objectFit="contain" />
+          <Image alt="" src={calendar} objectFit="contain" />
         </div>
         <h2 className="font-bold text-xl text-primary text-center">
           Sync up and set up before sunday 8pm for your upcoming week.
         </h2>
         <div className="max-w-md mx-4">
-          <Image src={work} objectFit="contain" />
+          <Image alt="" src={work} objectFit="contain" />
         </div>
         <p className="text-center mt-16">
           Decide on Budbudbud and let email flows sync up your calendars. Share
